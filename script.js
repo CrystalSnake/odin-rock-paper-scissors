db2d07e1ef89fe4c7d35723fa5c2c53c200e2871
Crystal Snake <22963880+CrystalSnake@users.noreply.github.com>
2022-11-07 23:05:51 +0400

    Add function for play single round

--- script.js
+++ script.js
@@ -1,7 +1,59 @@
-const handState = ["Rock", "Paper", "Scissors"];
+const handState = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
   return handState[Math.floor(Math.random() * 3)];
 }
 
-console.log(getComputerChoice());
+function getPlayerSelection() {
+  return prompt("Enter your choice (Rock, Paper or Scissors)").toLowerCase();
+}
+
+function playSingleRound(playerSelection, computerSelection) {
+  var computerSelection = getComputerChoice();
+  var playerSelection = getPlayerSelection();
+  if (playerSelection === "rock") {
+    if (computerSelection === "rock") {
+      console.log(
+        `Your choice ${playerSelection}, computer's choice ${computerSelection}. Draw!`
+      );
+    } else if (computerSelection === "paper") {
+      console.log(
+        `Your choice ${playerSelection}, computer's choice ${computerSelection}. You Lose!`
+      );
+    } else {
+      console.log(
+        `Your choice ${playerSelection}, computer's choice ${computerSelection}. You Win!`
+      );
+    }
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "paper") {
+      console.log(
+        `Your choice ${playerSelection}, computer's choice ${computerSelection}. Draw!`
+      );
+    } else if (computerSelection === "scissors") {
+      console.log(
+        `Your choice ${playerSelection}, computer's choice ${computerSelection}. You Lose!`
+      );
+    } else {
+      console.log(
+        `Your choice ${playerSelection}, computer's choice ${computerSelection}. You Win!`
+      );
+    }
+  } else {
+    if (computerSelection === "scissors") {
+      console.log(
+        `Your choice ${playerSelection}, computer's choice ${computerSelection}. Draw!`
+      );
+    } else if (computerSelection === "rock") {
+      console.log(
+        `Your choice ${playerSelection}, computer's choice ${computerSelection}. You Lose!`
+      );
+    } else {
+      console.log(
+        `Your choice ${playerSelection}, computer's choice ${computerSelection}. You Win!`
+      );
+    }
+  }
+}
+
+playSingleRound();
