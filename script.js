ac4f024a1d8eb54be5deb5aacc0c47bdd62c915a
Crystal Snake <22963880+CrystalSnake@users.noreply.github.com>
2022-11-08 19:31:35 +0400

    Refactor playSingleRound function

--- script.js
+++ script.js
@@ -9,51 +9,21 @@ function getPlayerSelection() {
 }
 
 function playSingleRound(playerSelection, computerSelection) {
-  var computerSelection = getComputerChoice();
-  var playerSelection = getPlayerSelection();
-  if (playerSelection === "rock") {
-    if (computerSelection === "rock") {
-      console.log(
-        `Your choice ${playerSelection}, computer's choice ${computerSelection}. Draw!`
-      );
-    } else if (computerSelection === "paper") {
-      console.log(
-        `Your choice ${playerSelection}, computer's choice ${computerSelection}. You Lose!`
-      );
-    } else {
-      console.log(
-        `Your choice ${playerSelection}, computer's choice ${computerSelection}. You Win!`
-      );
-    }
-  } else if (playerSelection === "paper") {
-    if (computerSelection === "paper") {
-      console.log(
-        `Your choice ${playerSelection}, computer's choice ${computerSelection}. Draw!`
-      );
-    } else if (computerSelection === "scissors") {
-      console.log(
-        `Your choice ${playerSelection}, computer's choice ${computerSelection}. You Lose!`
-      );
-    } else {
-      console.log(
-        `Your choice ${playerSelection}, computer's choice ${computerSelection}. You Win!`
-      );
-    }
-  } else {
-    if (computerSelection === "scissors") {
-      console.log(
-        `Your choice ${playerSelection}, computer's choice ${computerSelection}. Draw!`
-      );
-    } else if (computerSelection === "rock") {
-      console.log(
-        `Your choice ${playerSelection}, computer's choice ${computerSelection}. You Lose!`
-      );
-    } else {
-      console.log(
-        `Your choice ${playerSelection}, computer's choice ${computerSelection}. You Win!`
-      );
-    }
+  switch (true) {
+    case computerSelection === "rock" && playerSelection === "paper":
+    case computerSelection === "paper" && playerSelection === "scissors":
+    case computerSelection === "scissors" && playerSelection === "rock":
+      return `Your choice is ${playerSelection}, computer\'s choice is ${computerSelection}. You Win!`;
+    case computerSelection === "rock" && playerSelection === "scissors":
+    case computerSelection === "paper" && playerSelection === "rock":
+    case computerSelection === "scissors" && playerSelection === "paper":
+      return `Your choice is ${playerSelection}, computer\'s choice is ${computerSelection}. You Lose!`;
+    default:
+      return `Your choice is ${playerSelection}, computer\'s choice is ${computerSelection}. Draw!`;
   }
 }
 
-playSingleRound();
+const computerSelection = getComputerChoice();
+const playerSelection = getPlayerSelection();
+
+console.log(playSingleRound(playerSelection, computerSelection));
