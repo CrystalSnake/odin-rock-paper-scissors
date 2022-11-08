f81c2a8d965dd0ddd56a91669fd10ff560231ea8
Crystal Snake <22963880+CrystalSnake@users.noreply.github.com>
2022-11-08 20:03:41 +0400

    Add game function

--- script.js
+++ script.js
@@ -13,17 +13,40 @@ function playSingleRound(playerSelection, computerSelection) {
     case computerSelection === "rock" && playerSelection === "paper":
     case computerSelection === "paper" && playerSelection === "scissors":
     case computerSelection === "scissors" && playerSelection === "rock":
-      return `Your choice is ${playerSelection}, computer\'s choice is ${computerSelection}. You Win!`;
+      return [
+        `Your choice is ${playerSelection}, computer\'s choice is ${computerSelection}. You Win!`,
+        1,
+      ];
     case computerSelection === "rock" && playerSelection === "scissors":
     case computerSelection === "paper" && playerSelection === "rock":
     case computerSelection === "scissors" && playerSelection === "paper":
-      return `Your choice is ${playerSelection}, computer\'s choice is ${computerSelection}. You Lose!`;
+      return [
+        `Your choice is ${playerSelection}, computer\'s choice is ${computerSelection}. You Lose!`,
+        -1,
+      ];
     default:
-      return `Your choice is ${playerSelection}, computer\'s choice is ${computerSelection}. Draw!`;
+      return [
+        `Your choice is ${playerSelection}, computer\'s choice is ${computerSelection}. Draw!`,
+        0,
+      ];
   }
 }
 
-const computerSelection = getComputerChoice();
-const playerSelection = getPlayerSelection();
+function game() {
+  var playerScore = 0;
+  for (let i = 0; i < 5; i++) {
+    const computerSelection = getComputerChoice();
+    const playerSelection = getPlayerSelection();
+    console.log(playSingleRound(playerSelection, computerSelection)[0]);
+    playerScore += playSingleRound(playerSelection, computerSelection)[1];
+  }
+  if (playerScore === 0) {
+    console.log("Draw!");
+  } else if (playerScore > 0) {
+    console.log("You Win!");
+  } else {
+    console.log("You Lose!");
+  }
+}
 
-console.log(playSingleRound(playerSelection, computerSelection));
+game();
