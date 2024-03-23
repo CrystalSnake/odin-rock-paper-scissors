905a181009471ddb698e6a78eb16292df2273d14
CrystalSnake <22963880+CrystalSnake@users.noreply.github.com>
2024-03-23 16:11:40 +0400

    Refactoring

--- script.js
+++ script.js
@@ -1,73 +1,56 @@
 let playerChoice;
 let score = 0;
+const handState = ['rock', 'paper', 'scissors'];
+const maxCount = 3;
 
-const rock = document.getElementById('rock');
-rock.addEventListener('click', () => {
-  playerChoice = 'rock';
-});
-rock.addEventListener('click', playSingleRound);
-
-const paper = document.getElementById('paper');
-paper.addEventListener('click', () => {
-  playerChoice = 'paper';
-});
-paper.addEventListener('click', playSingleRound);
-
-const scissors = document.getElementById('scissors');
-scissors.addEventListener('click', () => {
-  playerChoice = 'scissors';
-});
-scissors.addEventListener('click', playSingleRound);
+const results = document.querySelector('.results');
+const roundResult = document.createElement('p');
+results.appendChild(roundResult);
+const scoreMessage = document.createElement('p');
+results.appendChild(scoreMessage);
+const gameResult = document.createElement('p');
+results.appendChild(gameResult);
 
 function getComputerChoice() {
-  const handState = ['rock', 'paper', 'scissors'];
-  let computerChoice = handState[Math.floor(Math.random() * 3)];
-  return computerChoice;
+  return handState[Math.floor(Math.random() * 3)];
 }
-
-const body = document.querySelector('body');
-const results = document.createElement('div');
-body.appendChild(results);
-const roundResult = document.createElement('p');
-const scoreMessage = document.createElement('p');
-const gameResult = document.createElement('p');
 
 function playSingleRound() {
   let computerChoice = getComputerChoice();
-  switch (true) {
-    case computerChoice === 'rock' && playerChoice === 'paper':
-    case computerChoice === 'paper' && playerChoice === 'scissors':
-    case computerChoice === 'scissors' && playerChoice === 'rock':
-      roundResult.textContent = `Your choice is ${playerChoice}, computer\'s choice is ${computerChoice}. You Win!`;
-      results.appendChild(roundResult);
-      score += 1;
-      scoreMessage.textContent = `Your score: ${score}`;
-      results.appendChild(scoreMessage);
-      break;
-    case computerChoice === 'rock' && playerChoice === 'scissors':
-    case computerChoice === 'paper' && playerChoice === 'rock':
-    case computerChoice === 'scissors' && playerChoice === 'paper':
-      roundResult.textContent = `Your choice is ${playerChoice}, computer\'s choice is ${computerChoice}. You Lose!`;
-      results.appendChild(roundResult);
-      score -= 1;
-      scoreMessage.textContent = `Your score: ${score}`;
-      results.appendChild(scoreMessage);
-      break;
-    default:
-      roundResult.textContent = `Your choice is ${playerChoice}, computer\'s choice is ${computerChoice}. Draw!`;
-      results.appendChild(roundResult);
-      scoreMessage.textContent = `Your score: ${score}`;
-      results.appendChild(scoreMessage);
-      break;
-  }
 
-  if (score > 4) {
+  const outcomes = {
+    rock: 'scissors',
+    paper: 'rock',
+    scissors: 'paper',
+  };
+
+  if (playerChoice === computerChoice) {
+    roundResult.textContent = `Your choice is ${playerChoice}, computer\'s choice is ${computerChoice}. Draw!`;
+  } else if (outcomes[playerChoice] === computerChoice) {
+    roundResult.textContent = `Your choice is ${playerChoice}, computer\'s choice is ${computerChoice}. You Win!`;
+    score += 1;
+  } else {
+    roundResult.textContent = `Your choice is ${playerChoice}, computer\'s choice is ${computerChoice}. You Lose!`;
+    score -= 1;
+  }
+  scoreMessage.textContent = `Your score: ${score}`;
+}
+
+function checkGameResult() {
+  if (score > maxCount - 1) {
     gameResult.textContent = 'You win game!';
-    results.appendChild(gameResult);
     score = 0;
-  } else if (score < -4) {
+  } else if (score < -maxCount - 1) {
     gameResult.textContent = 'You lose game!';
-    results.appendChild(gameResult);
     score = 0;
   }
 }
+
+handState.map((choice) => {
+  const element = document.getElementById(choice);
+  element.addEventListener('click', () => {
+    playerChoice = choice;
+    playSingleRound();
+    checkGameResult();
+  });
+});
